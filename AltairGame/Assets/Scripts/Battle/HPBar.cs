f4bec2b37237bbed15703f5216ac93e6067f2664
Bryan Isrrael Gavilanez Bacilio <bigavilanezb00@gmail.com>
2025-03-23 16:04:24 +0100

    Action selection, Move selection and attacks implemented
    Se ha implementado la interfaz para elegir movimientos de los Pokémon. Además se ha agregado la funcionalidad de atacar en los combates Pokémon. Falta por añadir que distinga entre ataque especial y ataque físico, que tenga en cuenta el los tipos al calcular el daño e implementar los criticos.

--- AltairGame/Assets/Scripts/Battle/HPBar.cs
+++ AltairGame/Assets/Scripts/Battle/HPBar.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class HPBar : MonoBehaviour
@@ -7,5 +8,19 @@ public class HPBar : MonoBehaviour
     public void SetHP(float hpNormalized)
     {
         health.transform.localScale = new Vector3(hpNormalized, 1f);
+    }
+
+    public IEnumerator SetHPSmooth(float newHp)
+    {
+        float curHp = health.transform.localScale.x;
+        float changeAmt = curHp - newHp;
+
+        while (curHp - newHp > Mathf.Epsilon)
+        {
+            curHp -= changeAmt * Time.deltaTime;
+            health.transform.localScale = new Vector3(curHp, 1f);
+            yield return null;
+        }
+        health.transform.localScale = new Vector3(newHp, 1f);
     }
 }
